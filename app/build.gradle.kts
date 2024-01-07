cf310e0867e3dd4e4aece36104434a237fd19e2f
obehi234 <obehi.ataga234@gmail.com>
2024-01-07 12:13:11 +0100

    Added hodenhof library dependency for circular images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //Hoddenhof circular-image-library
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+
 }
